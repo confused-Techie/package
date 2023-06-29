544af8d675c3f6bac16835a0bfd3e3922a41d248
confused_techie <dev@lhbasics.com>
2023-06-29 11:07:31 -0700

    First look at much more flexible structure

---
+++ lib-test/package-test.js
@@ -0,0 +1,15 @@
+const { describe, it } = require("node:test");
+const assert = require("node:assert");
+
+const Package = require("../lib/package.js");
+
+describe("simple test", () => {
+  it("does things", () => {
+    let pack = new Package({ pack: { author: "confused-Techie" }});
+
+    //console.log(pack);
+    assert.strictEqual(pack.author.field, "confused-Techie");
+    pack.author.field = "Daeraxa";
+    assert.strictEqual(pack.author.field, "Daeraxa");
+  });
+});
