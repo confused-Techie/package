6a4b22127a040bfb91a3775d950ae5c276e340dc
confused_techie <dev@lhbasics.com>
2023-06-30 09:25:01 -0700

    Field validation

--- lib-test/package-test.js
+++ lib-test/package-test.js
@@ -5,11 +5,12 @@ const Package = require("../lib/package.js");
 
 describe("simple test", () => {
   it("does things", () => {
-    let pack = new Package({ pack: { author: "confused-Techie" }});
+    let pack = new Package({ pack: { author: "confused-Techie", description: "Hello world" }});
 
     //console.log(pack);
     assert.strictEqual(pack.author.field, "confused-Techie");
     pack.author.field = "Daeraxa";
     assert.strictEqual(pack.author.field, "Daeraxa");
+    assert.strictEqual(pack.description.field, "Hello world");
   });
 });
